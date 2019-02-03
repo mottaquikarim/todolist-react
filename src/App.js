5c9855238db0d2addcd7eabdf72fd910f77712f5
Mottaqui Karim <taqqui.karim@gmail.com>
2019-02-03 12:49:50 -0500

    added addToList function that runs the todolist.addTodo but within a component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 
+import {addTodo, removeTodo, updateTodo} from './todolist';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        todos: [],
+    }
+
+    setTimeout(() => {
+        this.addToList('testing', true, Date.now() + 1000*60*60*24)
+    }, 2000);
+  }
+
+  addToList(name, isCompleted, dueDate) {
+    const newTodos = addTodo(this.state.todos, name, isCompleted, dueDate);
+    this.setState({
+        todos: newTodos,
+    })
+  }
+
   render() {
+    console.log('todos', this.state.todos)
     return (
       <div className="App">
         <header className="App-header">
